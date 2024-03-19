95a4da3f2cef6fa5b6e1ce605318789491180f90
Максим Хоменков <maksimhomenkov@yandex.ru>
2024-03-19 22:13:15 +0300

    добавлены photoStruct, photoRequestResult, ImagesListService

--- ImageFeed/ImagesList/ImagesListService.swift
+++ ImageFeed/ImagesList/ImagesListService.swift
@@ -6,3 +6,78 @@
 //
 
 import Foundation
+
+final class ImagesListService {
+    private var lastLoadedPage: Int?
+    private var task: URLSessionTask?
+    private (set) var photos: [Photo] = []
+    private let urlSession = URLSession.shared
+    private let oauth2TokenStorage = OAuth2TokenStorage()
+    static let shared = ImagesListService()
+    static let didChangeNotification = Notification.Name(rawValue: "ImagesListServiceDidChange")
+    private lazy var iso8601Formatter: ISO8601DateFormatter = {
+        let formatter = ISO8601DateFormatter()
+        return formatter
+    }()
+    
+    func fetchPhotosNextPage() {
+        let nextPage = (lastLoadedPage ?? 0) + 1
+        guard
+            let request = createURLRequest(page: nextPage),
+            task == nil
+        else { return }
+        assert(Thread.isMainThread)
+        task = URLSession.shared.objectTask(for: request) { [weak self] (response: Result<[PhotoResult], Error>) in
+            guard let self = self else { return }
+            switch response {
+            case .success(let body):
+                DispatchQueue.main.async {
+                    var photos: [Photo] = []
+                    body.forEach { photo in
+                        photos.append(self.convertToPhoto(photo))
+                    }
+                    self.photos += photos
+                    NotificationCenter.default.post(name: ImagesListService.didChangeNotification, object: self)
+                }
+                self.task = nil
+                self.lastLoadedPage = nextPage
+            case .failure(let error):
+                self.task = nil
+                print("[ImagesListService]: \(error.localizedDescription) \(request)")
+            }
+        }
+    }
+    
+    private func createURLRequest(page: Int) -> URLRequest? {
+        var urlComponents = URLComponents()
+        urlComponents.scheme = "https"
+        urlComponents.host = "api.unsplash.com"
+        urlComponents.path = "/photos"
+        urlComponents.queryItems = [
+            URLQueryItem(name: "page", value: String(page)),
+        ]
+        guard let url = urlComponents.url else { return nil }
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        if let token = oauth2TokenStorage.token {
+            request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+        }
+        return request
+    }
+    
+    private func convertToPhoto(_ photoResult: PhotoResult) -> Photo {
+        let result = Photo(
+            id: photoResult.id,
+            size: CGSize(width: photoResult.width, height: photoResult.height),
+            createdAt: self.iso8601Formatter.date(from: photoResult.created ?? ""),
+            welcomeDescription: photoResult.description,
+            thumbImageURL: photoResult.urls.small,
+            largeImageURL: photoResult.urls.full,
+            isLiked: photoResult.likedByUser)
+        return result
+    }
+    
+    func deletePhotos() {
+        photos.removeAll()
+    }
+}
--- ImageFeed/ImagesList/PhotoRequestResult.swift
+++ ImageFeed/ImagesList/PhotoRequestResult.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+struct PhotoResult: Codable {
+    var id: String
+    var width: Int
+    var height: Int
+    var created: String?
+    var description: String?
+    var urls: UrlsResult
+    var likedByUser: Bool
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case width = "width"
+        case height = "height"
+        case created = "created_at"
+        case description = "description"
+        case urls = "urls"
+        case likedByUser = "liked_by_user"
+    }
+}
+
+struct UrlsResult: Codable {
+    var raw: String
+    var full: String
+    var regular: String
+    var small: String
+    var thumb: String
+}
